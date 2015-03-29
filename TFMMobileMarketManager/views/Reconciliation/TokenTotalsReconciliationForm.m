eee44802f6c58132ac1cd7d8bdbc7df17d714a63
Alex Lindeman <aelindeman@gmail.com>
2015-03-29 17:14:10 -0400

    add token colors to redemptions and token totals

--- TFMMobileMarketManager/views/Reconciliation/TokenTotalsReconciliationForm.m
+++ TFMMobileMarketManager/views/Reconciliation/TokenTotalsReconciliationForm.m
@@ -10,13 +10,13 @@
 - (NSArray *)fields
 {
 	return @[
-		@{FXFormFieldKey: @"marketCreditTokenCount", FXFormFieldTitle: @"Credit tokens", FXFormFieldHeader: @"Market Tokens"},
-		@{FXFormFieldKey: @"marketSnapTokenCount", FXFormFieldTitle: @"SNAP tokens"},
-		@{FXFormFieldKey: @"marketBonusTokenCount", FXFormFieldTitle: @"Bonus tokens"},
+		@{FXFormFieldKey: @"marketCreditTokenCount", FXFormFieldTitle: @"Credit tokens", @"imageView.image": [UIImage imageNamed:@"token-green"], FXFormFieldHeader: @"Market Tokens"},
+		@{FXFormFieldKey: @"marketSnapTokenCount", FXFormFieldTitle: @"SNAP tokens", @"imageView.image": [UIImage imageNamed:@"token-blue"]},
+		@{FXFormFieldKey: @"marketBonusTokenCount", FXFormFieldTitle: @"Bonus tokens", @"imageView.image": [UIImage imageNamed:@"token-red"]},
 		
-		@{FXFormFieldKey: @"redeemedCreditTokenCount", FXFormFieldTitle: @"Credit tokens", FXFormFieldHeader: @"Redeemed Tokens"},
-		@{FXFormFieldKey: @"redeemedSnapTokenCount", FXFormFieldTitle: @"SNAP tokens"},
-		@{FXFormFieldKey: @"redeemedBonusTokenCount", FXFormFieldTitle: @"Bonus tokens"},
+		@{FXFormFieldKey: @"redeemedCreditTokenCount", FXFormFieldTitle: @"Credit tokens", @"imageView.image": [UIImage imageNamed:@"token-green"], FXFormFieldHeader: @"Redeemed Tokens"},
+		@{FXFormFieldKey: @"redeemedSnapTokenCount", FXFormFieldTitle: @"SNAP tokens", @"imageView.image": [UIImage imageNamed:@"token-blue"]},
+		@{FXFormFieldKey: @"redeemedBonusTokenCount", FXFormFieldTitle: @"Bonus tokens", @"imageView.image": [UIImage imageNamed:@"token-red"]},
 	];
 }
 
--- TFMMobileMarketManager/views/Redemptions/RedemptionForm.m
+++ TFMMobileMarketManager/views/Redemptions/RedemptionForm.m
@@ -23,9 +23,9 @@
 		@{FXFormFieldKey: @"date", FXFormFieldDefaultValue: [NSDate date]},
 		@{FXFormFieldTitle: @"Check number", FXFormFieldKey: @"check_number", FXFormFieldDefaultValue: @"1000"},
 		
-		@{FXFormFieldTitle: @"SNAP token count", FXFormFieldKey: @"snap_count", FXFormFieldAction: @"updateTokenTotals:", FXFormFieldHeader: @"Token counts"},
-		@{FXFormFieldTitle: @"Bonus token count", FXFormFieldKey: @"bonus_count", FXFormFieldAction: @"updateTokenTotals:"},
-		@{FXFormFieldTitle: @"Credit token count", FXFormFieldKey: @"credit_count", FXFormFieldAction: @"updateTokenTotals:"},
+		@{FXFormFieldTitle: @"Credit token count", FXFormFieldKey: @"credit_count", @"imageView.image": [UIImage imageNamed:@"token-green"], FXFormFieldAction: @"updateTokenTotals:", FXFormFieldHeader: @"Token counts"},
+		@{FXFormFieldTitle: @"SNAP token count", FXFormFieldKey: @"snap_count", @"imageView.image": [UIImage imageNamed:@"token-blue"], FXFormFieldAction: @"updateTokenTotals:"},
+		@{FXFormFieldTitle: @"Bonus token count", FXFormFieldKey: @"bonus_count", @"imageView.image": [UIImage imageNamed:@"token-red"], FXFormFieldAction: @"updateTokenTotals:"},
 		
 		@{FXFormFieldTitle: @"Credit token value", FXFormFieldKey: @"credit_amount", FXFormFieldType: FXFormFieldTypeLabel, FXFormFieldHeader: @"Token totals"},
 		@{FXFormFieldTitle: @"Total value", FXFormFieldKey: @"total", FXFormFieldType: FXFormFieldTypeLabel},
