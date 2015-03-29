dbf039d19970c591947c7e6336db242d0166a9e1
Alex Lindeman <aelindeman@gmail.com>
2015-03-29 15:56:08 -0400

    better sanitization, consolidated placeholder functions

--- TFMMobileMarketManager/views/Reconciliation/ReconciliationEntryTableViewCell.h
+++ TFMMobileMarketManager/views/Reconciliation/ReconciliationEntryTableViewCell.h
@@ -21,29 +21,21 @@
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 
 @property (weak, nonatomic) IBOutlet UITextField *snapField1;
-- (IBAction)snapField1EditingDidBegin:(id)sender;
-- (IBAction)snapField1EditingDidEnd:(id)sender;
 @property (weak, nonatomic) IBOutlet UITextField *snapField2;
-- (IBAction)snapField2EditingDidBegin:(id)sender;
-- (IBAction)snapField2EditingDidEnd:(id)sender;
-
 @property (weak, nonatomic) IBOutlet UITextField *creditField1;
-- (IBAction)creditField1EditingDidBegin:(id)sender;
-- (IBAction)creditField1EditingDidEnd:(id)sender;
 @property (weak, nonatomic) IBOutlet UITextField *creditField2;
-- (IBAction)creditField2EditingDidBegin:(id)sender;
-- (IBAction)creditField2EditingDidEnd:(id)sender;
-
 @property (weak, nonatomic) IBOutlet UITextField *totalField1;
-- (IBAction)totalField1EditingDidBegin:(id)sender;
-- (IBAction)totalField1EditingDidEnd:(id)sender;
 @property (weak, nonatomic) IBOutlet UITextField *totalField2;
-- (IBAction)totalField2EditingDidBegin:(id)sender;
-- (IBAction)totalField2EditingDidEnd:(id)sender;
+
+- (IBAction)sanitizeField:(id)sender;
+
+- (void)removePlaceholders;
 
 - (void)setField1Prefix:(NSString *)prefix;
 - (void)setField1Suffix:(NSString *)suffix;
 - (void)setField2Prefix:(NSString *)prefix;
 - (void)setField2Suffix:(NSString *)suffix;
+
+- (NSDictionary *)getData;
 
 @end
--- TFMMobileMarketManager/views/Reconciliation/ReconciliationEntryTableViewCell.m
+++ TFMMobileMarketManager/views/Reconciliation/ReconciliationEntryTableViewCell.m
@@ -13,7 +13,7 @@
 	UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
 	[label setBackgroundColor:[UIColor clearColor]];
 	[label setFont:[UIFont fontWithName:self.font.fontName size:self.font.pointSize]];
-	[label setTextColor:self.textColor];
+	[label setTextColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0.33]];
 	[label setText:prefix];
 	
 	CGSize prefixSize = [prefix sizeWithAttributes:@{NSFontAttributeName: label.font}];
@@ -28,7 +28,7 @@
 	UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
 	[label setBackgroundColor:[UIColor clearColor]];
 	[label setFont:[UIFont fontWithName:self.font.fontName size:self.font.pointSize]];
-	[label setTextColor:self.textColor];
+	[label setTextColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0.33]];
 	[label setText:suffix];
 	
 	CGSize suffixSize = [suffix sizeWithAttributes:@{NSFontAttributeName: label.font}];
@@ -110,12 +110,7 @@
 	return field2Suffix ? field2Suffix : @"";
 }
 
-// removes anything that isn't a number, because the iPad is dumb and keyboard will always have letter keys
-- (void)sanitizeField:(UITextField *)field
-{
-	if ([field text]) [field setText:[self sanitize:[field text]]];
-}
-
+// strips non-numeric characters from string
 - (NSString *)sanitize:(NSString *)string
 {
 	return [[string componentsSeparatedByCharactersInSet:[[NSCharacterSet characterSetWithCharactersInString:@"0123456789"] invertedSet]] componentsJoinedByString:@""];
@@ -134,55 +129,35 @@
 	};
 }
 
-// easy way to set the fields
-- (void)setData:(NSDictionary *)input
+- (void)removePlaceholders
 {
-	[self.snapField1 setText:[self sanitize:[input valueForKey:@"snapField1"]]];
-	[self.snapField2 setText:[self sanitize:[input valueForKey:@"snapField2"]]];
-	[self.creditField1 setText:[self sanitize:[input valueForKey:@"creditField1"]]];
-	[self.creditField2 setText:[self sanitize:[input valueForKey:@"creditField2"]]];
-	[self.totalField1 setText:[self sanitize:[input valueForKey:@"totalField1"]]];
-	[self.totalField2 setText:[self sanitize:[input valueForKey:@"totalField2"]]];
+	[self.snapField1 setPlaceholder:@""];
+	[self.snapField2 setPlaceholder:@""];
+	[self.creditField1 setPlaceholder:@""];
+	[self.creditField2 setPlaceholder:@""];
+	[self.totalField1 setPlaceholder:@""];
+	[self.totalField2 setPlaceholder:@""];
 }
 
-// append label Suffix and suffix
-- (void)appendUnitsTo:(UITextField *)field prefix:(NSString *)prefix suffix:(NSString *)suffix
+// quietly strip non-numeric characters from string rather than annoyingly validate
+- (IBAction)sanitizeField:(id)sender
 {
-	if ([field.text length] > 0)
+	if ([sender text])
+		[sender setText:[self sanitize:[sender text]]];
+}
+
+- (IBAction)needsPrefixAndSuffix:(id)sender
+{
+	if ([[sender restorationIdentifier] hasSuffix:@"Field1"])
 	{
-		[field setPrefixText:prefix];
-		[field setSuffixText:suffix];
+		[sender setPrefixText:self.field1Prefix];
+		//[sender setSuffixText:self.field1Suffix];
+	}
+	else if ([[sender restorationIdentifier] hasSuffix:@"Field2"])
+	{
+		[sender setPrefixText:self.field2Prefix];
+		//[sender setSuffixText:self.field2Suffix];
 	}
 }
-
-- (void)removeUnitsFrom:(UITextField *)field
-{
-	[field setLeftView:nil];
-	[field setRightView:nil];
-}
-
-// snapField1
-- (IBAction)snapField1EditingDidBegin:(id)sender { [self removeUnitsFrom:self.snapField1]; }
-- (IBAction)snapField1EditingDidEnd:(id)sender { [self sanitizeField:self.snapField1]; [self appendUnitsTo:self.snapField1 prefix:self.field1Prefix suffix:self.field1Suffix]; }
-
-// snapField2
-- (IBAction)snapField2EditingDidBegin:(id)sender { [self removeUnitsFrom:self.snapField2]; }
-- (IBAction)snapField2EditingDidEnd:(id)sender { [self sanitizeField:self.snapField2]; [self appendUnitsTo:self.snapField2 prefix:self.field2Prefix suffix:self.field2Suffix]; }
-
-// creditField1
-- (IBAction)creditField1EditingDidBegin:(id)sender { [self removeUnitsFrom:self.creditField1]; }
-- (IBAction)creditField1EditingDidEnd:(id)sender { [self sanitizeField:self.creditField1]; [self appendUnitsTo:self.creditField1 prefix:self.field1Prefix suffix:self.field1Suffix]; }
-
-// creditField2
-- (IBAction)creditField2EditingDidBegin:(id)sender { [self removeUnitsFrom:self.creditField2]; }
-- (IBAction)creditField2EditingDidEnd:(id)sender { [self sanitizeField:self.creditField2]; [self appendUnitsTo:self.creditField2 prefix:self.field2Prefix suffix:self.field2Suffix]; }
-
-// totalField1
-- (IBAction)totalField1EditingDidBegin:(id)sender { [self removeUnitsFrom:self.totalField1]; }
-- (IBAction)totalField1EditingDidEnd:(id)sender { [self sanitizeField:self.totalField1]; [self appendUnitsTo:self.totalField1 prefix:self.field1Prefix suffix:self.field1Suffix]; }
-
-// totalField2
-- (IBAction)totalField2EditingDidBegin:(id)sender { [self removeUnitsFrom:self.totalField2]; }
-- (IBAction)totalField2EditingDidEnd:(id)sender { [self sanitizeField:self.totalField2]; [self appendUnitsTo:self.totalField2 prefix:self.field2Prefix suffix:self.field2Suffix]; }
 
 @end
