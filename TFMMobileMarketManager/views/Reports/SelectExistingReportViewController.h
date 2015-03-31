ea6d735db7425d3b8906cae26d38a58bca4e7348
Alex Lindeman <aelindeman@gmail.com>
2015-03-31 01:43:57 -0400

    select existing reports

--- TFMMobileMarketManager/views/Reports/SelectExistingReportViewController.h
+++ TFMMobileMarketManager/views/Reports/SelectExistingReportViewController.h
@@ -5,9 +5,19 @@
 
 #import "AppDelegate.h"
 #import "ReportsMenuViewController.h"
+#import "ReportViewerViewController.h"
 
 @interface SelectExistingReportViewController : UITableViewController <ReportsMenuDelegate>
 
 @property (nonatomic, assign) id <ReportsMenuDelegate> delegate;
+
+// starting directory - will only traverse this folder for its immediate subfolders and their files
+@property NSString *basePath;
+
+@property NSString *selectedObject;
+
+// array of dictionaries
+// NSArray *items = @[ @{@"name": @"subfolder name", @"items": @[ @"file 1", @"file 2", ... ] }, @{ ... } ];
+@property NSArray *items;
 
 @end
--- TFMMobileMarketManager/views/Reports/SelectExistingReportViewController.m
+++ TFMMobileMarketManager/views/Reports/SelectExistingReportViewController.m
@@ -7,25 +7,76 @@
 
 @implementation SelectExistingReportViewController
 
-/*
-
-only need if displaying modally
+static NSString *deleteConfirmationMessageTitle = @"";
+static NSString *deleteConfirmationMessageDetails = @"";
 
 - (void)viewDidLoad
 {
 	[super viewDidLoad];
-	//[self load];
-	
-	UIBarButtonItem *closeButton = [[UIBarButtonItem alloc] initWithTitle:@"Close" style:UIBarButtonItemStylePlain target:self action:@selector(discard)];
-	UIBarButtonItem *openButton = [[UIBarButtonItem alloc] initWithTitle:@"Open" style:UIBarButtonItemStyleDone target:self action:@selector(open)];
-	
-	self.navigationItem.leftBarButtonItem = closeButton;
-	self.navigationItem.rightBarButtonItem = openButton;
+	if (!self.basePath) [self setBasePath:[NSString pathWithComponents:@[[TFM_DELEGATE.applicationDocumentsDirectory path], @"Reports"]]];
+	[self load];
 }
 
-- (void)discard
+- (void)load
 {
-	[self dismissViewControllerAnimated:true completion:nil];
-} */
+	NSAssert(self.basePath, @"self.basePath must be set");
+
+	NSError *error;
+	NSFileManager *fm = [NSFileManager defaultManager];
+	NSArray *subs = [fm contentsOfDirectoryAtPath:self.basePath error:&error];
+	
+	NSMutableArray *items = [[NSMutableArray alloc] init];
+	for (NSString *subfolder in subs)
+	{
+		NSArray *contents = [[fm contentsOfDirectoryAtPath:[NSString pathWithComponents:@[self.basePath, subfolder]] error:&error] filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"self endswith '.csv'"]];
+		[items addObject:@{ @"name": subfolder, @"items": contents }];
+	}
+	
+	[self setItems:items];
+	[self.tableView reloadData];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+	return [self.items count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	return [[(NSDictionary *)[self.items objectAtIndex:section] objectForKey:@"items"] count];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+	return [[self.items objectAtIndex:section] valueForKey:@"name"];
+}
+
+// populate the table view
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ReportCell"];
+	NSString *name = [[[self.items objectAtIndex:indexPath.section] objectForKey:@"items"] objectAtIndex:indexPath.row];
+	[cell.textLabel setText:name];
+	return cell;
+}
+
+// trigger segue on selection
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	NSString *marketday = [[self.items objectAtIndex:indexPath.section] valueForKey:@"name"];
+	NSString *name = [[[self.items objectAtIndex:indexPath.section] objectForKey:@"items"] objectAtIndex:indexPath.row];
+	
+	NSString *resolvedPath = [NSString pathWithComponents:@[self.basePath, marketday, name]];
+	NSLog(@"opening report at %@", resolvedPath);
+	
+	[self performSegueWithIdentifier:@"ReportViewerSegue" sender:resolvedPath];
+	[tableView deselectRowAtIndexPath:indexPath animated:true];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+	if ([segue.identifier isEqualToString:@"ReportViewerSegue"])
+		[(ReportViewerViewController *)segue.destinationViewController setFilePath:sender];
+}
 
 @end
