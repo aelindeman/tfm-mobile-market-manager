d941e657e302f7da67fefeaadb07c2436becc352
Alex Lindeman <aelindeman@gmail.com>
2015-04-06 02:03:33 -0400

    import button iboutlet

--- TFMMobileMarketManager/views/Import/ImporterViewController.h
+++ TFMMobileMarketManager/views/Import/ImporterViewController.h
@@ -16,6 +16,7 @@
 
 @property (weak, nonatomic) IBOutlet UISegmentedControl *importDestination;
 @property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UIButton *importButton;
 
 - (void)handleOpenURL:(NSURL *)url;
 - (IBAction)prepopulatePrompt:(id)sender;
